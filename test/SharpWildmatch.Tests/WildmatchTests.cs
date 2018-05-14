8097829abd69b9a9749d7c54f00ccd298892fc58
Paul Knopf <pauldotknopf@gmail.com>
2018-05-14 12:43:57 -0400

    Finished tests.

--- test/SharpWildmatch.Tests/WildmatchTests.cs
+++ test/SharpWildmatch.Tests/WildmatchTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using FluentAssert;
 using Xunit;
 
 namespace SharpWildmatch.Tests
@@ -201,82 +199,29 @@ namespace SharpWildmatch.Tests
             Test(MatchResult.Match, MatchResult.Match, MatchResult.Match, MatchResult.Match, @"Z", @"[Z-y]");
         }
         
-        [Fact]
-        public void Basic_wildmatch()
+        private void Test(
+            // ReSharper disable ParameterOnlyUsedForPreconditionCheck.Local
+            MatchResult wildmatch,
+            MatchResult wildmatchInsenstive,
+            MatchResult pathmatch,
+            MatchResult pathmatchInsensitive,
+            // ReSharper restore ParameterOnlyUsedForPreconditionCheck.Local
+            string input,
+            string pattern)
         {
-            //# Basic wildTest(features
-            Test(true, true, true, true, "foo", "foo");
-            Test(false, false, false, false, "foo", "bar");
-            Test(true, true, true, true, "", "");
-            Test(true, true, true, true, "foo", "???");
-            Test(false, false, false, false, "foo", "??");
-            Test(true, true, true, true, "foo", "*");
-            Test(true, true, true, true, "foo", "f*");
-            Test(false, false, false, false, "foo", "*f");
-            Test(true, true, true, true, "foo", "*foo*");
-            Test(true, true, true, true, "foobar", "*ob*a*r*");
-            Test(true, true, true, true, "aaaaaaabababab", "*ab");
-            Test(true, true, true, true, "foo*", "foo\\*");
-            Test(false, false, false, false, "foobar", "foo\\*bar");
-            Test(true, true, true, true, "f\\oo", "f\\\\oo");
-            Test(true, true, true, true, "ball", "*[al]?");
-            Test(false, false, false, false, "ten", "[ten]");
-            // TODO
-            //Test(false, false, true, true, "ten", "**[!te]");
-            Test(false, false, false, false, "ten", "**[!ten]");
-            Test(true, true, true, true, "ten", "t[a-g]n");
-            Test(false, false, false, false, "ten", "t[!a-g]n");
-            Test(true, true, true, true, "ton", "t[!a-g]n");
-            Test(true, true, true, true, "ton", "t[^a-g]n");
-            Test(true, true, true, true, "a]b", "a[]]b");
-            Test(true, true, true, true, "a-b", "a[]-]b");
-            Test(true, true, true, true, "a]b", "a[]-]b");
-            Test(false, false, false, false, "aab", "a[]-]b");
-            Test(true, true, true, true, "aab", "a[]a-]b");
-            Test(true, true, true, true, "]", "]");
-        }
+            var wildmatchResult = Wildmatch.Match(pattern, input, MatchFlags.None);
+            var wildmatchInsenstiveResult = Wildmatch.Match(pattern, input, MatchFlags.CaseFold);
+            var pathmatchResult = Wildmatch.Match(pattern, input, MatchFlags.PathName);
+            var pathmatchInsensitiveResult = Wildmatch.Match(pattern, input, MatchFlags.PathName | MatchFlags.CaseFold);
 
-        private void Test(bool wildmatch, bool wildmatchInsenstive, bool pathmatch, bool pathmatchInsensitive, string input, string pattern)
-        {
-            Wildmatch.Match(pattern, input, MatchFlags.None)
-                .ShouldBeEqualTo(wildmatch, $"Rule (wildmatch={wildmatch}) for input: '{input}' pattern: '{pattern}' failed.");
-
-            Wildmatch.Match(pattern, input, MatchFlags.CaseFold)
-                .ShouldBeEqualTo(wildmatchInsenstive, $"Rule (wildmatchi={wildmatchInsenstive}) for input: '{input}' pattern: '{pattern}' failed.");
-
-            Wildmatch.Match(pattern, input, MatchFlags.PathName)
-                .ShouldBeEqualTo(pathmatch, $"Rule (pathmatch={pathmatch}) for input: '{input}' pattern: '{pattern}' failed.");
-
-            Wildmatch.Match(pattern, input, MatchFlags.PathName | MatchFlags.CaseFold)
-                .ShouldBeEqualTo(pathmatchInsensitive, $"Rule (pathmatchi={pathmatchInsensitive}) for input: '{input}' pattern: '{pattern}' failed.");
-        }
-
-        private void TestGitListFiles(bool wildmatch, string input, string pattern)
-        {
-            using (var directory = new WorkingDirectorySession())
+            try
             {
-                SimpleExec.Command.Run("git", "init", directory.Directory);
-
-                if (!string.IsNullOrEmpty(input))
-                {
-                    if (!string.IsNullOrEmpty(Path.GetDirectoryName(input)))
-                    {
-                        Directory.CreateDirectory(Path.Combine(directory.Directory, Path.GetDirectoryName(input)));
-                    }
-
-                    File.WriteAllText(Path.Combine(directory.Directory, input), "test");
-                }
-
-                var output = SimpleExec.Command.Read("git", "--glob-pathspecs ls-files --others -z", directory.Directory);
-
-                if (!wildmatch)
-                {
-                    output.ShouldNotBeNullOrEmpty();
-                }
-                else
-                {
-                    output.StartsWith(input).ShouldBeTrue();
-                }
+                Assert.Equal($"{wildmatch}:{wildmatchInsenstive}:{pathmatch}:{pathmatchInsensitive}",
+                    $"{wildmatchResult}:{wildmatchInsenstiveResult}:{pathmatchResult}:{pathmatchInsensitiveResult}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Input: {input} Pattern: {pattern} Message: {ex.Message}");
             }
         }
     }
